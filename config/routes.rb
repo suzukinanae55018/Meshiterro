1a4cefd9eaa363448709418a1534103fd9db34d9
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-05-13 11:47:59 +0000

    [Finish] 4-13

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root to: "homes#top"
-
-  resources :post_images, only: [:new, :create, :index, :show]
-
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
   get 'homes/about' => 'homes#about', as: :about
   # 'homes/about'というURLを指定すると、homescontrollerのaboutアクションが実行される about_pathとして
