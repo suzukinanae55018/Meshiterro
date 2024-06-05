874355e21089ede5ab46e2dab3cc9959044f5c01
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-06-05 06:07:42 +0000

    [Add]RegistrationsControllerにdeviseを継承email許可

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: "homes#top"
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
